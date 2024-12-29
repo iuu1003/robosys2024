2ba1c9b137af87f762f1ab55aef0a3fd3e1c03f2
Miyuu Taniguchi <aiuiiuu1003@icloud.com>
2024-12-29 18:09:06 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2024 Miyuu Taniguchi
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,18 +9,61 @@ ng () {
 
 res=0
 
-### NOMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+### NORMAL INPUT ###
+cat > players.txt <<EOF
+Miyuu,5,2,1,0,1,0
+Chocolat,3,3,1,0,2,0
+Sally,4,3,2,1,1,0
+Jade,4,2,3,1,0,1
+Daniel,5,0,0,0,0,0
+Emily,4,2,1,1,0,0
+Jasmine,1,0,1,0,0,0
+EOF
+
+output=$(
+cat << 'EOF'
+試合のMVP判定結果
+1位: Sally (スコア:18)
+2位: Chocolat (スコア:13)
+3位: Jade (スコア:12)
+4位: Emily (スコア:11)
+5位: Miyuu (スコア:9)
+6位: Jasmine (スコア:3)
+7位: Daniel (スコア:0)
+
+今日のMVPはSally選手です
+EOF
+)
+
+out=$(cat players.txt | ./baseball_mvp)
+[ "${out}" = "$output" ] || ng "$LINENO"
+
+rm -f players.txt
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]       || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+#dataに含まれる要素が７つのときのみ反映
+cat > players.txt <<EOF
+Miyuu,5,2,1,0,1
+EOF
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+output="error: データ数が正しくありません"
+
+out=$(cat players.txt | ./baseball_mvp)
+[ "${out}" = "$output" ] || ng "$LINENO"
+
+rm -f players.txt
+
+#成績が数字のときのみ反映
+cat > players.txt <<EOF
+Miyuu,5,あ,1,0,1,0
+EOF
+
+output="error: 数値変換が正しくありません"
+
+out=$(cat players.txt | ./baseball_mvp)
+[ "${out}" = "$output" ] || ng "$LINENO"
+
+rm -f players.txt
 
 [ "${res}" = 0 ] && echo OK
 exit $res
